3c3f962251db40be9a8adbd1e63f4234f3f05c1f
samimeadad <sami.meadad@gmail.com>
2022-11-25 15:52:18 +0600

    Google Font, fontawesome and react-bootstrap integrated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'react-bootstrap';
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="m-5">
+      <h1>HOME</h1>
+      <div><Button>Click Me</Button></div>
     </div>
+
   );
 }
 
